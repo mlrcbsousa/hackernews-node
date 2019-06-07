3b3df0d79240ead5c74736a157d311266f58e81f
Manuel Sousa <mlrcbsousa@gmail.com>
2019-06-07 14:33:13 +0200

    deleteLink mutation

--- src/index.js
+++ src/index.js
@@ -32,8 +32,9 @@ const resolvers = {
       return link;
     },
     deleteLink: (parent, args) => {
-      const link = links[args.id];
-      delete link;
+      const index = links.findIndex(l => l.id === args.id);
+      const link = links[index];
+      links.splice(index, 1);
       return link;
     }
   }
