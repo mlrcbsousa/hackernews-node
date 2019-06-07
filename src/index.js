12592f187a662ed59ba042c1eb7646059837d5fc
Manuel Sousa <mlrcbsousa@gmail.com>
2019-06-07 16:06:24 +0200

    prisma configuration

--- src/index.js
+++ src/index.js
@@ -1,42 +1,35 @@
 const { GraphQLServer } = require('graphql-yoga')
 
 // dummy data
-let links = [{
-  id: 'link-0',
-  url: 'www.howtographql.com',
-  description: 'Fullstack tutorial for GraphQL'
-}]
 
-let idCount = links.length
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links,
-    link: (parent, args) => links.find(l => l.id === args.id)
+    feed: (root, args, context, info) => {
+      return context.prisma.links();
+    },
+    // link: (parent, args) => links.find(l => l.id === args.id)
   },
   Mutation: {
-    post: (parent, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
+    post: (root, args, context) => {
+      return context.prisma.createLink({
         url: args.url
-      };
-      links.push(link);
-      return link;
+        description: args.description,
+      });
     },
-    updateLink: (parent, args) => {
-      const link = links.find(link => link.id === args.id);
-      for (const arg in args) {
-        if (args[arg] && arg !== "id") { link[arg] = args[arg] };
-      };
-      return link;
-    },
-    deleteLink: (parent, args) => {
-      const index = links.findIndex(l => l.id === args.id);
-      const link = links[index];
-      links.splice(index, 1);
-      return link;
-    }
+    // updateLink: (parent, args) => {
+    //   const link = links.find(link => link.id === args.id);
+    //   for (const arg in args) {
+    //     if (args[arg] && arg !== "id") { link[arg] = args[arg] };
+    //   };
+    //   return link;
+    // },
+    // deleteLink: (parent, args) => {
+    //   const index = links.findIndex(l => l.id === args.id);
+    //   const link = links[index];
+    //   links.splice(index, 1);
+    //   return link;
+    // }
   }
 }
 
